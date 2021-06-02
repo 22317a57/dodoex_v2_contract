b6d559f8489ff84aa320b0cfab81d6784644058f
owen05 <liuhongnan53@163.com>
2021-06-02 10:41:26 +0800

    deploy nft on rinkeby

--- config/rinkeby-config.js
+++ config/rinkeby-config.js
@@ -1,23 +1,33 @@
 module.exports = {
     RINKEBY_CONFIG: {
+        //Template
+        CloneFactory: "0x823ECBfCCD3e6Cb67d4c9334F743BEe0E60A7349",
+
         //================== NFT ====================
-        ConstFeeRateModel: "",
-        FeeDistributor: "",
-        Fragment: "",
-        NFTCollateralVault: "",
-        DODONFTRouteHelper: "",
+        Fragment: "0x20f7205c4addF7D4Bf8521754d57003013bC49d2",
+        NFTCollateralVault: "0x23d72eA97a9E43411Eeb908d128DF337aD334582",
+        DODONFTRouteHelper: "0xb0Ca341b6fbdC607A507D821780e29f9601a58B3",
 
-        InitializableERC721: "",
-        InitializableERC1155: "",
-        NFTTokenFactory: "",
+        InitializableERC721: "0xC0ccfC832BD45Cd3A2d62e47FE92Fc50DD2210ac",
+        InitializableERC1155: "0x9DC9086B65cCBec43F92bFa37Db81150Ed1DDDed",
+        NFTTokenFactory: "0xd2BffcCBC1F2a7356f8DaBB55B33E47D62de1bB1",
 
-        DODONFTRegistry: "",
-        DODONFTProxy: "",
+        DodoNftErc721: "0x3Bc20358B31aD498d5a245B36bC993DDBE9A4405",
+        DodoNftErc1155: "0xc498F36eF82Aa5dBE0ecF6DD56DD55398E80E13D",
 
+        DODONFTRegistry: "0x69efeCA5070Cb22c1094cffEbacafC09c058c139",
+        DODONFTProxy: "", //暂无
 
-        //================= MysteryBox =================
-        MysteryBoxV1: "",
-        RandomGenerator: "0x7e21BFAcDB5062C071Fad17451E85f070DDa5d8F",
-        RandomPool: []
+        //================= DropsV1 =================
+        MysteryBoxV1: "0x94a2eF99CD4F11b57Daaf688183eae536B3fbBe9",
+        RandomGenerator: "0x69C8a7fc6E05d7aa36114B3e35F62DEcA8E11F6E",
+        RandomPool: [],
+
+        //================= DropsV2 ==================
+        DropsFeeModel: "",
+        DropsProxy: "",
+
+        // DropsERC721: "0x3df8d553275781C777f432A74EEE9099226B9d13",
+        // DropsERC1155: "0x3a8EcF30428bd4e33Cd7011533DFd596F7705c8F",
     }
 }
